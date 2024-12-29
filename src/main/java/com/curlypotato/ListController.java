8d8ddf4a33635f03df4e096c550d475c5b521443
Rebecca Hellström Karlsson <rebecca.hellstrom.karlsson@omegapoint.se>
2024-12-29 11:21:20 +0100

    Make hello world controller to be on /hello

--- src/main/java/com/curlypotato/ListController.java
+++ src/main/java/com/curlypotato/ListController.java
@@ -5,7 +5,7 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
 
-@Controller
+@Controller("/hello")
 public class ListController {
     
     @Get
