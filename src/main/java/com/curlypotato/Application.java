017e6e46506b63f46b1359c1e0daaf0914bcb214
Rebecca Hellström Karlsson <rebecca.hellstrom.karlsson@omegapoint.se>
2025-01-05 12:17:30 +0100

    Add CORS debug settings

--- src/main/java/com/curlypotato/Application.java
+++ src/main/java/com/curlypotato/Application.java
@@ -5,6 +5,7 @@ import io.micronaut.runtime.Micronaut;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("Starting Micronaut Application...");
         Micronaut.run(Application.class, args);
     }
 }
